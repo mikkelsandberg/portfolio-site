8c2a6199e44f7d72da3ea35a5a6ccc8cc2ea7d81
Mikkel <mikkel.h.sandberg@gmail.com>
2018-06-19 17:19:36 -0700

    adding new files

---
+++ mikkel-sandberg-portfolio-2018/src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./AboutMe.css";
+
+class AboutMe extends Component {
+	render() {
+		return (
+			<section id="aboutMeWrapper">
+				<header>
+					<h1>About Me</h1>
+				</header>
+			</section>
+		);
+	}
+}
+
+export default AboutMe;
---
+++ mikkel-sandberg-portfolio-2018/src/Components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+	render() {
+		return (
+			<section id="contactWrapper">
+				<header>
+					<h1>Contact</h1>
+				</header>
+			</section>
+		);
+	}
+}
+
+export default Contact;
