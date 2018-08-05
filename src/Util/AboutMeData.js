a3096e3e9408d77cac55622bb538a0dc386d3820
Mikkel Sandberg <mikkel.h.sandberg@gmail.com>
2018-08-04 17:04:54 -0700

    changing profile pic

--- src/Util/AboutMeData.js
+++ src/Util/AboutMeData.js
@@ -1,30 +1,27 @@
 const AboutMeData = {
-	profilePic:
-		'https://s3-us-west-1.amazonaws.com/msandberg-portfolio-2018/mikkel-beach-square.jpg',
+	profilePic: 'https://s3-us-west-1.amazonaws.com/msandberg-portfolio-2018/mikkel-portrait-med.jpg',
 	stats: [
 		{
 			stat: 'Mikkel Sandberg',
-			icon: 'user',
+			icon: 'user'
 		},
 		{
 			stat: 'Web Developer,<br>Designer, and<br>Podcast Producer',
-			icon: 'briefcase',
+			icon: 'briefcase'
 		},
 		{
 			stat: 'California, US',
-			icon: 'map-marker-alt',
+			icon: 'map-marker-alt'
 		},
 		{
-			stat:
-				"<a href='https://github.com/MikkelSandbag' target='_blank' rel='noopener noreferrer'>Github profile</a>",
-			icon: ['fab', 'github'],
-		},
+			stat: "<a href='https://github.com/MikkelSandbag' target='_blank' rel='noopener noreferrer'>Github profile</a>",
+			icon: ['fab', 'github']
+		}
 	],
-	introText:
-		'Hi there! My name is Mikkel and I am a web developer and podcast producer based in California.',
+	introText: 'Hi there! My name is Mikkel and I am a web developer and podcast producer based in California.',
 	bodyContent: [
-		'Most of my <a href="/my-work">professional work</a> has been as a front end web developer, but I have a curiosity and hunger to learn. From supporting a community of professionals by <a href="/my-work/experts-exchange-live-product-redesign">improving user experience</a> and building <a href="/my-work/apc-entertainment-website-design-and-development">websites for small businesses</a>, to <a href="/my-work/friend-quest-podcast-production">making podcasts</a> to tell a good story, my guiding principle is to make sure that my work makes a positive impact on the world.',
-	],
+		'Most of my <a href="/my-work">professional work</a> has been as a front end web developer, but I have a curiosity and hunger to learn. From supporting a community of professionals by <a href="/my-work/experts-exchange-live-product-redesign">improving user experience</a> and building <a href="/my-work/apc-entertainment-website-design-and-development">websites for small businesses</a>, to <a href="/my-work/friend-quest-podcast-production">making podcasts</a> to tell a good story, my guiding principle is to make sure that my work makes a positive impact on the world.'
+	]
 };
 
 export default AboutMeData;
