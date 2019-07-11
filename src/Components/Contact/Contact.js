59e587d08f380d1443ef14e256b921963ce7afcc
Mikkel Sandberg <mikkel.h.sandberg@gmail.com>
2019-07-11 10:22:53 -0700

    updating contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,7 +4,7 @@ import './Contact.css';
 function Contact() {
 	return (
 		<section className="contact__wrapper">
-			<form action="https://formspree.io/hello@mikkelsandberg.com" method="POST" className="contact__form">
+			<form action="https://usebasin.com/f/31dde79131f1" method="POST" className="contact__form">
 				<input
 					className="contact__form__input contact__form__input--name"
 					type="text"
@@ -15,14 +15,14 @@ function Contact() {
 				<input
 					className="contact__form__input contact__form__input--email"
 					type="email"
-					name="_replyto"
+					name="email"
 					placeholder="your@email.com"
 					required
 				/>
 				<input
 					className="contact__form__input contact__form__input--subject"
 					type="text"
-					name="_subject"
+					name="subject"
 					placeholder="Subject"
 					required
 				/>
@@ -34,6 +34,7 @@ function Contact() {
 					className="contact__form__input contact__form__input--message"
 					required
 				/>
+				<input type="hidden" name="_gotcha" />
 				<button className="contact__form__input contact__form__input--submit" type="submit">Send</button>
 			</form>
 		</section>
